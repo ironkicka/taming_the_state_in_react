84d7aa612b288284dfaef8a912bb3bc70f6230c1
Shunsuke Hioki <iron.kicka624@gmail.com>
2021-07-15 10:52:49 +0900

    reduxの記述を追加、アーカイブ機能を追加

--- react-redux-hackernews/src/index.js
+++ react-redux-hackernews/src/index.js
@@ -3,31 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import store from './store';
+import { getReadableStories} from "./selectors/story";
+import {STORY_ARCHIVE} from "./constants/actionTypes";
 
-const stories = [
-    {
-        title:"React",
-        url:"https://facebook.github.io/react/",
-        author:'Jordan Walke',
-        num_comments:3,
-        points:4,
-        objectID:0
-    },
-    {
-        title:'Redux',
-        url:'https://github.com/reactjs/redux',
-        author:'Dan Abramov, Andrew Clark',
-        num_comments:2,
-        points:5,
-        objectID: 1,
-    }
-]
-ReactDOM.render(
-  <React.StrictMode>
-    <App stories={stories} onArchive={()=>{}}/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+function render() {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App
+                stories={getReadableStories(store.getState())}
+                onArchive={id => store.dispatch({type: STORY_ARCHIVE, id})}
+            />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
+store.subscribe(render);
+render();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
