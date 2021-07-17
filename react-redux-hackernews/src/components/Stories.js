973fc52db0897230f356e1d38dcb6796c089f1ae
Shunsuke Hioki <iron.kicka624@gmail.com>
2021-07-17 10:45:27 +0900

    エラーハンドリングを追加

--- react-redux-hackernews/src/components/Stories.js
+++ react-redux-hackernews/src/components/Stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Stories.css';
 
 import Story from './Story';
-import {getReadableStories} from "../selectors/story";
+import {getFetchError, getReadableStories} from "../selectors/story";
 import {doArchiveStory} from "../actions/archive";
 import {connect} from "react-redux";
 
@@ -28,9 +28,10 @@ const COLUMNS = {
     },
 };
 
-const Stories = ({stories}) =>
+const Stories = ({stories,error}) =>
     <div className="stories">
         <StoriesHeader columns={COLUMNS}/>
+        {error && <p className={"error"}>Something went wrong ...</p>}
         {(stories || []).map(story =>
             <Story
                 key={story.objectID}
@@ -54,7 +55,8 @@ const StoriesHeader = ({columns}) => [
 ]
 
 const mapStateToProps = state =>({
-    stories:getReadableStories(state)
+    stories:getReadableStories(state),
+    error:getFetchError(state)
 });
 
 export default connect(
